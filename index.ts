b580c795e21a88bbf3261352f7d22a8bfa6bd63b
Sarbajit Biswal <sarbajit.biswal@comviva.com>
2023-06-20 18:11:52 +0530

    fixed headers not coming response

--- index.ts
+++ index.ts
@@ -9,16 +9,23 @@ app.use(cors());
 const port = Number(process.env.PORT) || 5000;
 app.use("*", async (req, res, next) => {
   try {
-  console.log(`Base Request URL: ${req.baseUrl}`);
-  const requestMethod: HttpType = HttpType[req.method as keyof typeof HttpType];
+    console.log(`Base Request URL: ${req.baseUrl}`);
+    const requestMethod: HttpType =
+      HttpType[req.method as keyof typeof HttpType];
     const response = await prisma.post.findFirstOrThrow({
       where: {
-        endpoint: {
-          contains: req.baseUrl,
+        mockURL: {
+          search: req.baseUrl,
         },
-        type: requestMethod
+        type: requestMethod,
       },
+      include : {
+        CustomHeaders: true
+      }
     });
+    if (response.CustomHeaders.length != 0 ) {
+      response.CustomHeaders.map((item) => res.setHeader(item.key, item.value))
+    }
     res.status(response.statusCode).json(JSON.parse(response.response));
     next();
   } catch (error) {
